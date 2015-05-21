f2d97eed8d5ad4307d546f2a70e01ee25fd3004c
Bruno Bonte <bruno.bonte@gmail.com>
2015-05-21 05:48:53 +0200

    added back-end

---
+++ server/models/Client.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+// Create the ClientSchema
+var ClientSchema = new mongoose.Schema({
+  cpf: {
+    type: String,
+    required: true
+  }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  maritalStatus: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  phoneNumbers: {
+    type: [String]
+    required: true
+  }
+});
+
+// Export the model Schema
+module.exports = ClientSchema;
